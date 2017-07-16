1d0ad7714f083936f3a71b559615e37a6a179f55
nemanjoza946 <n.damnjanovic33@hotmail.com>
2017-07-16 12:25:10 +0000

    Add user data to articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,7 @@ class ArticlesController < ApplicationController
     
   end
   
-  def create 
-    debugger
+  def create
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
